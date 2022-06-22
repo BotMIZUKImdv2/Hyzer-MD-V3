9d5e67775d7b98b35a134cce2082fef03264e1bd
BotMIZUKImdv2 <102995614+BotMIZUKImdv2@users.noreply.github.com>
2022-06-23 07:22:35 +0800

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn, text, participants }) => {
   let teks = `⋙ *T̳A̳G̳A̳L̳L̳ ̳M̳E̳M̳* ⋘
 \n *${text ? text : 'Nothing'}*\n\n`
 		      	for (let mem of participants) {
-		            teks +=💂` @${mem.id.split('@')[0]}\n`
+		            teks += ` @${mem.id.split('@')[0]}\n`
 				}
                 teks += `\nAlwiOfc`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
